fd88524775620f51bfe12870555aa2d712eb8f37
theTechnoKid <thetechnokid11@gmail.com>
2016-02-15 19:45:44 -0500

    Basic game files

---
+++ src/dev/thetechnokid/rw/RocketWarfare.java
@@ -0,0 +1,31 @@
+package dev.thetechnokid.rw;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class RocketWarfare extends Application {
+
+	public static final String NAME = "Rocket Warfare";
+	public static final String VERSION = "Pre-Alpha";
+	public static final int BUILD_NUM = 0;
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		AnchorPane parent = FXMLLoader.load(getClass().getResource("fxml/Game.fxml"));
+		Scene scene = new Scene(parent);
+
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.sizeToScene();
+		stage.setTitle(NAME + " | " + VERSION);
+		stage.show();
+	}
+
+}
