56ad45a2a48307ccba12bb0ade390c20b86d864b
theTechnoKid <thetechnokid11@gmail.com>
2016-02-15 20:02:19 -0500

    Quick fix

--- src/dev/thetechnokid/rw/controllers/MainGameController.java
+++ src/dev/thetechnokid/rw/controllers/MainGameController.java
@@ -10,16 +10,16 @@ import javafx.scene.paint.Color;
 public class MainGameController implements Initializable {
 
 	@FXML
-	private Canvas canvas;
+	private Canvas theCanvas;
 	
 	private GraphicsContext g;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		System.out.println("adf");
-		g = canvas.getGraphicsContext2D();
+		g = theCanvas.getGraphicsContext2D();
 		g.setFill(Color.AQUA);
-		g.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		g.fillRect(0, 0, theCanvas.getWidth(), theCanvas.getHeight());
 	}
 
 }
