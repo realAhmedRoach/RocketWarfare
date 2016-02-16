23ea3d9264079319d77981a9ff436ebef978bcf2
theTechnoKid <thetechnokid11@gmail.com>
2016-02-15 20:09:19 -0500

    Main game loop

--- src/dev/thetechnokid/rw/controllers/MainGameController.java
+++ src/dev/thetechnokid/rw/controllers/MainGameController.java
@@ -3,23 +3,38 @@ package dev.thetechnokid.rw.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.*;
 import javafx.fxml.*;
 import javafx.scene.canvas.*;
 import javafx.scene.paint.Color;
+import javafx.util.Duration;
 
 public class MainGameController implements Initializable {
 
 	@FXML
 	private Canvas theCanvas;
-	
+
 	private GraphicsContext g;
-	
+
+	Timeline gameLoop = new Timeline();
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
-		System.out.println("adf");
 		g = theCanvas.getGraphicsContext2D();
-		g.setFill(Color.AQUA);
-		g.fillRect(0, 0, theCanvas.getWidth(), theCanvas.getHeight());
+
+		gameLoop.setCycleCount(Timeline.INDEFINITE);
+		// final long timeStart = System.currentTimeMillis();
+
+		KeyFrame kf = new KeyFrame(Duration.seconds(0.017), // 60 FPS
+				(event) -> {
+					g.clearRect(0, 0, theCanvas.getWidth(), theCanvas.getHeight());
+
+					g.setFill(Color.AQUA);
+					g.fillRect(0, 0, theCanvas.getWidth(), theCanvas.getHeight());
+				});
+
+		gameLoop.getKeyFrames().add(kf);
+		gameLoop.play();
 	}
 
 }
