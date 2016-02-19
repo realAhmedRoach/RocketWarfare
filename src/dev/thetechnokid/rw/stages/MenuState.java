d94e4a8e18811fe62f442139619b3b5abb6f2a4d
theTechnoKid <thetechnokid11@gmail.com>
2016-02-19 15:20:58 -0500

    Some more tests

--- src/dev/thetechnokid/rw/stages/MenuState.java
+++ src/dev/thetechnokid/rw/stages/MenuState.java
@@ -5,21 +5,30 @@ import dev.thetechnokid.rw.utils.Grid;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 public class MenuState extends State {
 
+	private boolean textOn;
+	
 	public MenuState(GraphicsContext g) {
 		super(g);
 	}
 
 	@Override
 	protected void init() {
-		MainGameController.get().buttons.getChildren().add(new Button("hey"));
+		Button b = new Button("hey");
+		b.setOnAction((event) -> textOn =! textOn);
+		MainGameController.get().buttons.getChildren().add(b);
+		
+		g.setFont(new Font("Consolas", 20));
 	}
 	
 	@Override
 	public void render() {
 		g.setStroke(Color.RED);
+		g.setFill(Color.DARKGRAY);
+		if(textOn) g.fillText("hey", 50, 30);
 		Grid.render(g);
 	}
 
