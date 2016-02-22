dad0417cabdf194ad798a111e45b0e5a4680de70
theTechnoKid <thetechnokid11@gmail.com>
2016-02-21 19:33:25 -0500

    Starting menu controls

--- src/dev/thetechnokid/rw/states/MenuState.java
+++ src/dev/thetechnokid/rw/states/MenuState.java
@@ -2,15 +2,16 @@ package dev.thetechnokid.rw.states;
 
 import dev.thetechnokid.rw.controllers.MainGameController;
 import dev.thetechnokid.rw.utils.Grid;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 
 public class MenuState extends State {
 
 	private boolean textOn;
+	private StringProperty nameProp;
 
 	public MenuState(GraphicsContext g) {
 		super(g);
@@ -18,27 +19,31 @@ public class MenuState extends State {
 
 	@Override
 	protected void init() {
-		Button b = new Button("hey");
-		b.setOnAction((event) -> textOn = !textOn);
-		MainGameController.get().buttons().add(b);
-
-		g.setFont(new Font("Consolas", 20));
+		Label name = new Label("Name:");
+		TextField nameField = new TextField();
+		nameField.setPromptText("JSmith");
+		nameField.setMaxWidth(100);
+		nameProp = nameField.textProperty();
+		Button start = new Button("Start Building!");
+		start.setOnAction((event) -> textOn = true);
+		
+		MainGameController.buttons().addAll(name, nameField, start);
 	}
 
 	@Override
 	public void render() {
 		g.setStroke(Color.RED);
-		g.setFill(Color.DARKGRAY);
+		g.setFill(Color.BLUEVIOLET);
 		if (textOn)
-			g.fillText("hey", 50, 30);
+			g.fillText("Welcome, " + nameProp.get(), 50, 30);
 		Grid.render(g);
 	}
 
 	@Override
 	public void tick() {
-		if (MainGameController.get().getMouse().isMousePressed()) {
-			int x = MainGameController.get().getMouse().getX();
-			int y = MainGameController.get().getMouse().getY();
+		if (MainGameController.getMouse().isMousePressed()) {
+			int x = MainGameController.getMouse().getX();
+			int y = MainGameController.getMouse().getY();
 			System.out.println(x + "," + y);
 			Point2D p = Grid.getGridLocation(x, y);
 			g.fillRect(p.getX(), p.getY(), Grid.SIZE, Grid.SIZE);
