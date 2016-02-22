6f5c90949279ef341e38f00fb28228b7b6ee91ad
theTechnoKid <thetechnokid11@gmail.com>
2016-02-22 14:09:08 -0500

    Added Utils centerText()

---
+++ src/dev/thetechnokid/rw/utils/Utils.java
@@ -0,0 +1,18 @@
+package dev.thetechnokid.rw.utils;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.Font;
+
+public class Utils {
+	public static void centerText(GraphicsContext g, String text, int size) {
+		int width = (int) g.getCanvas().getWidth();
+		int height = (int) g.getCanvas().getHeight();
+		int totalSize = text.length() * size;
+		
+		int x = (width / 2) - (totalSize/2);
+		int y = (height / 2) - (totalSize/2);
+		
+		g.setFont(new Font("Consolas", size));
+		g.fillText(text, x, y);
+	}
+}
