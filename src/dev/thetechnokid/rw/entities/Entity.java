887643a861ba9b39e651fb941bb403938f8c095c
theTechnoKid <thetechnokid11@gmail.com>
2016-02-22 19:32:13 -0500

    Entity has image

--- src/dev/thetechnokid/rw/entities/Entity.java
+++ src/dev/thetechnokid/rw/entities/Entity.java
@@ -1,11 +1,13 @@
 package dev.thetechnokid.rw.entities;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 
 public abstract class Entity {
 	public int x, y;
 	public int w, h;
 	protected GraphicsContext g;
+	protected Image image;
 
 	public Entity(GraphicsContext g) {
 		this.g = g;
