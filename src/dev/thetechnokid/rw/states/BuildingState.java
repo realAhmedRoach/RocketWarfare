5025329622be4419a3ce0cdd4a39461577e59a02
theTechnoKid <thetechnokid11@gmail.com>
2016-02-26 12:40:58 -0500

    Added building state

---
+++ src/dev/thetechnokid/rw/states/BuildingState.java
@@ -0,0 +1,30 @@
+package dev.thetechnokid.rw.states;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class BuildingState extends State {
+
+	public BuildingState(GraphicsContext g) {
+		super(g);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
