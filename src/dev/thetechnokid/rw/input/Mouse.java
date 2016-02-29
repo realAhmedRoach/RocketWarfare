0ebb3cb7c9dcca1b11024dc7cc048fdbd1439925
theTechnoKid <thetechnokid11@gmail.com>
2016-02-28 19:46:43 -0500

    getPointOnGrid()

--- src/dev/thetechnokid/rw/input/Mouse.java
+++ src/dev/thetechnokid/rw/input/Mouse.java
@@ -1,6 +1,7 @@
 package dev.thetechnokid.rw.input;
 
 import dev.thetechnokid.rw.controllers.MainGameController;
+import dev.thetechnokid.rw.utils.Grid;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.input.*;
@@ -19,7 +20,8 @@ public class Mouse implements EventHandler<MouseEvent> {
 			if (x > MainGameController.getWidth() || x < 0) {
 				event.consume();
 				mousePressed = false;
-			} if (y > MainGameController.getHeight() || y < 0) {
+			}
+			if (y > MainGameController.getHeight() || y < 0) {
 				event.consume();
 				mousePressed = false;
 			}
@@ -42,5 +44,9 @@ public class Mouse implements EventHandler<MouseEvent> {
 
 	public Point2D getPoint() {
 		return new Point2D(getX(), getY());
+	}
+
+	public Point2D getPointOnGrid() {
+		return new Point2D(getX() / Grid.SIZE, getY() / Grid.SIZE);
 	}
 }
