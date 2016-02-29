4f37b7dd6382c967cd8e35bb775c50ba7c3161b8
theTechnoKid <thetechnokid11@gmail.com>
2016-02-28 19:52:06 -0500

    Drawing

--- src/dev/thetechnokid/rw/states/BuildingState.java
+++ src/dev/thetechnokid/rw/states/BuildingState.java
@@ -1,30 +1,34 @@
 package dev.thetechnokid.rw.states;
 
+import java.util.HashMap;
+
 import dev.thetechnokid.rw.controllers.MainGameController;
 import dev.thetechnokid.rw.utils.*;
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 
 public class BuildingState extends State {
-	private int x,y;
+	private int x, y;
+	private HashMap<Point2D, Boolean> locs = new HashMap<>();
 	private Color color = Color.RED;
 	private Animator anim;
 	private int recty;
-	
+
 	public BuildingState(GraphicsContext g) {
 		super(g);
-		
+
 	}
 
 	@Override
 	protected void init() {
 		MainGameController.buttons().clear();
 		g.setFill(Color.RED);
-		
-		anim = new Animator(1000, () -> recty+=Grid.SIZE);
-		
+
+		anim = new Animator(1000, () -> recty += Grid.SIZE);
+
 		Button b = new Button("Change Colors");
 		b.setOnAction((event) -> {
 			if (color.equals(Color.RED))
@@ -33,7 +37,7 @@ public class BuildingState extends State {
 				color = (Color.RED);
 			g.getCanvas().requestFocus();
 		});
-		
+
 		MainGameController.buttons().add(b);
 	}
 
@@ -43,6 +47,13 @@ public class BuildingState extends State {
 		g.fillRect(x, y, Grid.SIZE, Grid.SIZE);
 		g.setFill(Color.BEIGE);
 		g.fillRect(0, recty, Grid.SIZE, Grid.SIZE);
+
+		g.setFill(Color.CADETBLUE);
+		for (Point2D p : locs.keySet()) {
+			if (locs.get(p)) {
+				g.fillRect(p.getX() * Grid.SIZE, p.getY() * Grid.SIZE, Grid.SIZE, Grid.SIZE);
+			}
+		}
 	}
 
 	@Override
@@ -58,6 +69,9 @@ public class BuildingState extends State {
 		}
 		if (MainGameController.getKeyboard().get(KeyCode.LEFT)) {
 			x -= Grid.SIZE;
+		}
+		if (MainGameController.getMouse().isMousePressed()) {
+			locs.put(MainGameController.getMouse().getPointOnGrid(), true);
 		}
 		anim.tick();
 	}
