9c49707404acce412106a3c1d7d8094e08b9b234
theTechnoKid <thetechnokid11@gmail.com>
2016-02-29 12:20:19 -0500

    wait() in Utils

--- src/dev/thetechnokid/rw/utils/Utils.java
+++ src/dev/thetechnokid/rw/utils/Utils.java
@@ -14,4 +14,9 @@ public class Utils {
 		g.setFont(new Font("Consolas", size));
 		g.fillText(text, x, y);
 	}
+	
+	public static void wait(int millisecs) {
+		long startTime = System.currentTimeMillis();
+		while (System.currentTimeMillis() - startTime < millisecs) {}
+	}
 }
