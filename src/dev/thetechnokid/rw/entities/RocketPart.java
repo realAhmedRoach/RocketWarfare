e9d78b7e564c1ab6abcdaf71fe1adba507fde377
theTechnoKid <thetechnokid11@gmail.com>
2016-03-05 12:08:52 -0500

    Simple RocketPart class

---
+++ src/dev/thetechnokid/rw/entities/RocketPart.java
@@ -0,0 +1,64 @@
+package dev.thetechnokid.rw.entities;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
+public class RocketPart extends Entity {
+
+	private int type, weight;
+	private Point2D posInRocket;
+	private RocketPart north, south, east, west;
+
+	public RocketPart(GraphicsContext g) {
+		super(g);
+	}
+
+	@Override
+	public void render() {
+	}
+
+	@Override
+	public void tick() {
+	}
+
+	public Point2D getPosInRocket() {
+		return posInRocket;
+	}
+
+	public void setPosInRocket(Point2D posInRocket) {
+		this.posInRocket = posInRocket;
+	}
+
+	public RocketPart getNorth() {
+		return north;
+	}
+
+	public void setNorth(RocketPart north) {
+		this.north = north;
+	}
+
+	public RocketPart getSouth() {
+		return south;
+	}
+
+	public void setSouth(RocketPart south) {
+		this.south = south;
+	}
+
+	public RocketPart getEast() {
+		return east;
+	}
+
+	public void setEast(RocketPart east) {
+		this.east = east;
+	}
+
+	public RocketPart getWest() {
+		return west;
+	}
+
+	public void setWest(RocketPart west) {
+		this.west = west;
+	}
+
+}
