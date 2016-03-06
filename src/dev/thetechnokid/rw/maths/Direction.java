66d742949881b42bcadf668749e9d64bdb4a1b06
aarh9 <aarh9@bms-stu-29407.schls.albco>
2016-03-06 11:42:40 -0500

    VectorQuantity and Direction

---
+++ src/dev/thetechnokid/rw/maths/Direction.java
@@ -0,0 +1,5 @@
+package dev.thetechnokid.rw.maths;
+
+public enum Direction {
+	NORTH, SOUTH, EAST, WEST
+}
---
+++ src/dev/thetechnokid/rw/maths/VectorQuantity.java
@@ -0,0 +1,8 @@
+package dev.thetechnokid.rw.maths;
+
+public class VectorQuantity {
+
+	public int magnitude;
+	public Direction direction;
+
+}
