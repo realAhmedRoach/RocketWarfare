a84245cadf66020561eeb4a8d2a1b7ea72b2a33c
theTechnoKid <thetechnokid11@gmail.com>
2016-03-06 14:34:27 -0500

    Position class

---
+++ src/dev/thetechnokid/rw/maths/Position.java
@@ -0,0 +1,16 @@
+package dev.thetechnokid.rw.maths;
+
+public class Position {
+	public int x, altitude;
+	
+	public Position() {
+		x = 0;
+		altitude = 0;
+	}
+	
+	public Position(int x, int altitude) {
+		this.x = x;
+		this.altitude = altitude;
+	}
+
+}
