48b13d3bb2fc2843c9592294b269a012433f09a5
theTechnoKid <theTechnoKid11@gmail.com>
2016-03-07 08:42:06 -0500

    Angle fix

--- src/dev/thetechnokid/rw/maths/Direction.java
+++ src/dev/thetechnokid/rw/maths/Direction.java
@@ -4,7 +4,7 @@ public class Direction {
 	public int degrees;
 	
 	public static final Direction NORTH = new Direction(90);
-	public static final Direction SOUTH = new Direction(360);
+	public static final Direction SOUTH = new Direction(270);
 	public static final Direction EAST = new Direction(0);
 	public static final Direction WEST = new Direction(180);
 	
