f9589bfc0a96237d56330a48a5d51801180d0929
theTechnoKid <theTechnoKid11@gmail.com>
2016-03-08 08:22:09 -0500

    Fixed Altitude Modifier

--- src/dev/thetechnokid/rw/maths/Direction.java
+++ src/dev/thetechnokid/rw/maths/Direction.java
@@ -2,46 +2,42 @@ package dev.thetechnokid.rw.maths;
 
 public class Direction {
 	public int degrees;
-	
+
 	public static final Direction NORTH = new Direction(90);
 	public static final Direction SOUTH = new Direction(270);
 	public static final Direction EAST = new Direction(0);
 	public static final Direction WEST = new Direction(180);
-	
+
 	public Direction(int degrees) {
 		this.degrees = degrees;
 	}
-	
+
 	public boolean equals(Direction other) {
 		return this.degrees == other.degrees;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return degrees;
 	}
-	
+
 	public double getAltitudeModifier() {
 		double percent = 0;
-		int degreesAbs = 0;
-		if(degrees == 0 || degrees == 180) 
+		double degreesAbs = degrees;
+		if (degrees == 0 || degrees == 180)
 			return 0;
-		else if (degrees == 90) 
+		else if (degrees == 90)
 			return 1;
 		else if (degrees == 270)
 			return -1;
-		if (degrees <= 180) {
-			if(degrees > 90) {
-				degreesAbs = degrees - 90;
-			}
+		if (degrees < 90) {
 			percent = degreesAbs / 90;
-		} else if (degrees < 180) {
-			if (degrees < 270) {
-				degreesAbs = degrees - 180;
-			} else {
-				degreesAbs = degrees - 270;
-			}
-			percent = -degreesAbs / 90;
+		} else if (degrees > 90) {
+			degreesAbs = 180 - degrees;
+			percent = degreesAbs / 90;
+		} else if (degrees > 270) {
+			degreesAbs = (degrees - 270) - 90;
+			percent = degreesAbs / 90;
 		}
 		System.out.println(percent);
 		return percent;
