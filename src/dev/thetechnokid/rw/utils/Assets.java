d5070b1bd1afb214ff8db870e84a7cb0a1b39d22
theTechnoKid <thetechnokid11@gmail.com>
2016-03-14 21:31:09 -0400

    Formatting

--- src/dev/thetechnokid/rw/utils/Assets.java
+++ src/dev/thetechnokid/rw/utils/Assets.java
@@ -7,7 +7,7 @@ public class Assets {
 	private Assets() {
 	}
 
-	public static Image ROCKET_PARTS = new Image(RocketWarfare.class.getResourceAsStream("res/images/spritesheet.png"));;
+	public static Image ROCKET_PARTS = new Image(RocketWarfare.class.getResourceAsStream("res/images/spritesheet.png"));
 	
 	public static Image crop(Image image, int row, int col) {
 		// TODO: Make body
