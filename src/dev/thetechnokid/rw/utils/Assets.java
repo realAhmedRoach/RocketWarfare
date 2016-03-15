90450702eb7ab61ece64c420a7c19178be4bb531
theTechnoKid <thetechnokid11@gmail.com>
2016-03-14 21:41:51 -0400

    Made crop() method

--- src/dev/thetechnokid/rw/utils/Assets.java
+++ src/dev/thetechnokid/rw/utils/Assets.java
@@ -1,16 +1,24 @@
 package dev.thetechnokid.rw.utils;
 
+import java.awt.image.BufferedImage;
+
 import dev.thetechnokid.rw.RocketWarfare;
-import javafx.scene.image.Image;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.*;
 
 public class Assets {
 	private Assets() {
 	}
 
 	public static Image ROCKET_PARTS = new Image(RocketWarfare.class.getResourceAsStream("res/images/spritesheet.png"));
+	public static BufferedImage PARTS = null; 
 	
+	static {
+		PARTS = SwingFXUtils.fromFXImage(ROCKET_PARTS, PARTS);
+	}
 	public static Image crop(Image image, int row, int col) {
-		// TODO: Make body
-		return null;
+		BufferedImage img = PARTS.getSubimage(col * Grid.SIZE, row * Grid.SIZE, Grid.SIZE, Grid.SIZE);
+		WritableImage outImg = null;
+		return SwingFXUtils.toFXImage(img, outImg);
 	}
 }
