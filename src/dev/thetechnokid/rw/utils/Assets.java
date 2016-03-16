d7cc6b44c9a48f7e688dd30a1f99ace41d9558f0
theTechnoKid <thetechnokid11@gmail.com>
2016-03-16 17:21:48 -0400

    Fixed rotation

--- src/dev/thetechnokid/rw/utils/Assets.java
+++ src/dev/thetechnokid/rw/utils/Assets.java
@@ -6,6 +6,7 @@ import dev.thetechnokid.rw.RocketWarfare;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.transform.Rotate;
 
 public class Assets {
 	public static Image ROCKET_PARTS;
@@ -26,8 +27,13 @@ public class Assets {
 
 	public static void renderFlip(GraphicsContext g, Image image, int col, int row) {
 		g.save();
-		g.rotate(-180);
+		rotate(g, 270, (col * Grid.SIZE) + image.getWidth() / 2, (row * Grid.SIZE) + image.getHeight() / 2);
 		g.drawImage(image, col * Grid.SIZE, row * Grid.SIZE);
 		g.restore();
+	}
+
+	private static void rotate(GraphicsContext g, double angle, double px, double py) {
+		Rotate r = new Rotate(angle, px, py);
+		g.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
 	}
 }
