f865344c11196754159bc64098c29cf5680c3003
theTechnoKid <thetechnokid11@gmail.com>
2016-03-16 19:14:33 -0400

    Removed unnecessary code

--- src/dev/thetechnokid/rw/utils/Assets.java
+++ src/dev/thetechnokid/rw/utils/Assets.java
@@ -1,9 +1,6 @@
 package dev.thetechnokid.rw.utils;
 
-import java.awt.image.BufferedImage;
-
 import dev.thetechnokid.rw.RocketWarfare;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -11,19 +8,12 @@ import javafx.scene.transform.Rotate;
 
 public class Assets {
 	public static Image ROCKET_PARTS;
-	private static BufferedImage PARTS;
 
 	private Assets() {
 	}
 
 	public static void init() {
 		ROCKET_PARTS = new Image(RocketWarfare.class.getResourceAsStream("/images/spritesheet.png"));
-		PARTS = SwingFXUtils.fromFXImage(ROCKET_PARTS, null);
-	}
-
-	public static Image crop(Image image, int col, int row) {
-		BufferedImage img = PARTS.getSubimage(col * Grid.SIZE, row * Grid.SIZE, Grid.SIZE, Grid.SIZE);
-		return SwingFXUtils.toFXImage(img, null);
 	}
 
 	public static void renderCropped(GraphicsContext g, Image image, int col, int row, int x, int y) {
