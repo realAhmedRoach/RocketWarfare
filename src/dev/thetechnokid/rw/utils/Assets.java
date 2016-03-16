41dd00da7db4a82a8938bab138e49edcae193fed
theTechnoKid <thetechnokid11@gmail.com>
2016-03-15 20:38:45 -0400

    init() method in Assets

--- src/dev/thetechnokid/rw/utils/Assets.java
+++ src/dev/thetechnokid/rw/utils/Assets.java
@@ -7,12 +7,17 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.*;
 
 public class Assets {
-	public static Image ROCKET_PARTS = new Image(RocketWarfare.class.getResourceAsStream("res/images/spritesheet.png"));
-	private static BufferedImage PARTS = SwingFXUtils.fromFXImage(ROCKET_PARTS, null);
+	public static Image ROCKET_PARTS;
+	private static BufferedImage PARTS;
 
 	private Assets() {
 	}
 
+	public static void init() {
+		ROCKET_PARTS = new Image(RocketWarfare.class.getResourceAsStream("res/images/spritesheet.png"));
+		PARTS = SwingFXUtils.fromFXImage(ROCKET_PARTS, null);
+	}
+	
 	public static Image crop(Image image, int row, int col) {
 		BufferedImage img = PARTS.getSubimage(col * Grid.SIZE, row * Grid.SIZE, Grid.SIZE, Grid.SIZE);
 		return SwingFXUtils.toFXImage(img, null);
