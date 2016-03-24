b0c44265d4e104fb5bb87177865edacf8b834b22
theTechnoKid <thetechnokid11@gmail.com>
2016-03-24 18:26:44 -0400

    Remove unnecessary code

--- src/dev/thetechnokid/rw/utils/Assets.java
+++ src/dev/thetechnokid/rw/utils/Assets.java
@@ -3,10 +3,7 @@ package dev.thetechnokid.rw.utils;
 import java.nio.IntBuffer;
 
 import dev.thetechnokid.rw.RocketWarfare;
-import javafx.geometry.Point2D;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.*;
-import javafx.scene.transform.Rotate;
 
 public class Assets {
 	public static Image ROCKET_PARTS;
@@ -45,29 +42,4 @@ public class Assets {
 
 		return image;
 	}
-
-	public static void renderCropped(GraphicsContext g, Image image, int col, int row, int x, int y) {
-		g.drawImage(image, col * Grid.SIZE, row * Grid.SIZE, Grid.SIZE, Grid.SIZE, x, y, Grid.SIZE, Grid.SIZE);
-	}
-
-	public static void renderCropped(GraphicsContext g, Image image, int col, int row, Point2D pos) {
-		renderCropped(g, image, col, row, (int) pos.getX(), (int) pos.getY());
-	}
-
-	public static void renderFlip(GraphicsContext g, Image image, int col, int row, int x, int y, int angle) {
-		g.save();
-		rotate(g, angle, (col * Grid.SIZE) + Grid.SIZE / 2, (row * Grid.SIZE) + Grid.SIZE / 2);
-		renderCropped(g, image, col, row, x, y);
-		g.restore();
-	}
-
-	public static void renderFlip(GraphicsContext g, Image image, int col, int row, Point2D pos, int angle) {
-		renderFlip(g, image, col, row, (int) pos.getX(), (int) pos.getY(), angle);
-	}
-
-	private static void rotate(GraphicsContext g, double angle, double px, double py) {
-		Rotate r = new Rotate(angle, px, py);
-		g.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
-	}
-
 }
