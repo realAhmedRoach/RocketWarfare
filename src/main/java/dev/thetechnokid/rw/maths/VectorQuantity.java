12145020508b979b1030a8c761e06ac46a1907b8
theTechnoKid <thetechnokid11@gmail.com>
2016-04-12 07:03:48 -0400

    Formatted

--- src/main/java/dev/thetechnokid/rw/maths/VectorQuantity.java
+++ src/main/java/dev/thetechnokid/rw/maths/VectorQuantity.java
@@ -5,52 +5,53 @@ public class VectorQuantity {
 	private int magnitude;
 	private Direction direction;
 	private boolean isFinal;
-	
+
 	public static final VectorQuantity GRAVITY = new VectorQuantity(5, Direction.south());
 
 	public VectorQuantity() {
 		magnitude = 0;
 		direction = Direction.north();
 	}
-	
+
 	public VectorQuantity(int magnitude, Direction direction) {
 		this(magnitude, direction, false);
 	}
-	
+
 	public VectorQuantity(int magnitude, Direction direction, boolean isFinal) {
 		this.magnitude = magnitude;
 		this.direction = direction;
 		this.isFinal = isFinal;
 	}
-	
+
 	public boolean equals(VectorQuantity other) {
 		return (magnitude == other.magnitude) && (direction.equals(other.direction));
 	}
-	
+
 	public int getMagnitude() {
 		return magnitude;
 	}
-	
+
 	public void setMagnitude(int magnitude) {
-		if(isFinal)
+		if (isFinal)
 			return;
 		this.magnitude = magnitude;
 	}
-	
+
 	public Direction getDirection() {
 		return direction;
 	}
 
 	public void increaseMagnitude(int i) {
-		if(isFinal)
+		if (isFinal)
 			return;
 		magnitude += i;
 	}
 
 	public void decreaseMagnitude(int i) {
-		if(isFinal)
+		if (isFinal)
 			return;
-		if (magnitude == 0) return;
+		if (magnitude == 0)
+			return;
 		magnitude -= i;
 	}
 }
