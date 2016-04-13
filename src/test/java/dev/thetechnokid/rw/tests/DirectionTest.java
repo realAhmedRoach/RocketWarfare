dd4850c2ba308a1ebf964c5f979fb346c24e6f58
theTechnoKid <thetechnokid11@gmail.com>
2016-04-12 21:25:58 -0400

    Create junit tests

---
+++ src/test/java/dev/thetechnokid/rw/tests/DirectionTest.java
@@ -0,0 +1,19 @@
+package dev.thetechnokid.rw.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DirectionTest {
+
+	@Test
+	public void testGetAltitudeModifier() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetXModifier() {
+		fail("Not yet implemented");
+	}
+
+}
