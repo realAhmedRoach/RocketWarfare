3354167a06bc078a675bf94c1a934324184109ec
theTechnoKid <thetechnokid11@gmail.com>
2016-04-12 21:34:41 -0400

    Fix and format tests

--- src/test/java/dev/thetechnokid/rw/tests/DirectionTest.java
+++ src/test/java/dev/thetechnokid/rw/tests/DirectionTest.java
@@ -7,17 +7,19 @@ import org.junit.Test;
 import dev.thetechnokid.rw.maths.Direction;
 
 public class DirectionTest {
-	
+
 	private Direction d = new Direction(87);
-	
+
 	@Test
 	public void testGetAltitudeModifier() {
-		assertEquals("Incorrect Altitude modifier for QI", 87/90, d.getAltitudeModifier(), 0);
+		double mod = 87.0 / 90.0;
+		assertEquals("Incorrect Altitude modifier for QI", mod, d.getAltitudeModifier(), 0);
 	}
 
 	@Test
 	public void testGetXModifier() {
-		assertEquals("Incorrect X modifier for QI", 3/90, d.getXModifier(), 0);
+		double mod = 3.0 / 90.0;
+		assertEquals("Incorrect X modifier for QI", mod, d.getXModifier(), 0);
 	}
 
 }
