ff2a75eae0235bfa786656ad6e2dee06811c68d6
theTechnoKid <thetechnokid11@gmail.com>
2016-04-17 07:28:55 -0400

    Rocket is drawn rotated

--- src/main/java/dev/thetechnokid/rw/utils/Utils.java
+++ src/main/java/dev/thetechnokid/rw/utils/Utils.java
@@ -1,7 +1,9 @@
 package dev.thetechnokid.rw.utils;
 
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.text.Font;
+import javafx.scene.transform.Rotate;
 
 public class Utils {
 	public static void centerText(GraphicsContext g, String text, int size) {
@@ -20,4 +22,35 @@ public class Utils {
 		while (System.currentTimeMillis() - startTime < millisecs) {
 		}
 	}
+	
+	   /**
+     * Sets the transform for the GraphicsContext to rotate around a pivot point.
+     *
+     * @param g the graphics context the transform to applied to.
+     * @param angle the angle of rotation.
+     * @param px the x pivot co-ordinate for the rotation (in canvas co-ordinates).
+     * @param py the y pivot co-ordinate for the rotation (in canvas co-ordinates).
+     */
+    private static void rotate(GraphicsContext g, double angle, double px, double py) {
+        Rotate r = new Rotate(angle, px, py);
+        g.setTransform(r.getMxx(), r.getMyx(), r.getMxy(), r.getMyy(), r.getTx(), r.getTy());
+    }
+
+    /**
+     * Draws an image on a graphics context.
+     *
+     * The image is drawn at (tlpx, tlpy) rotated by angle pivoted around the point:
+     *   (tlpx + image.getWidth() / 2, tlpy + image.getHeight() / 2)
+     *
+     * @param g the graphics context the image is to be drawn on.
+     * @param angle the angle of rotation.
+     * @param tlpx the top left x co-ordinate where the image will be plotted (in canvas co-ordinates).
+     * @param tlpy the top left y co-ordinate where the image will be plotted (in canvas co-ordinates).
+     */
+    public static void drawRotatedImage(GraphicsContext g, Image image, double angle, double tlpx, double tlpy) {
+        g.save(); // saves the current state on stack, including the current transform
+        rotate(g, angle, tlpx + image.getWidth() / 2, tlpy + image.getHeight() / 2);
+        g.drawImage(image, tlpx, tlpy);
+        g.restore(); // back to original state (before rotation)
+    }
 }
