e35080f2210b9f7f01a57f528b30dad939caf08e
theTechnoKid <thetechnokid11@gmail.com>
2016-04-29 08:41:51 -0400

    Gravity acceleration force

--- src/main/java/dev/thetechnokid/rw/maths/Force.java
+++ src/main/java/dev/thetechnokid/rw/maths/Force.java
@@ -1,6 +1,8 @@
 package dev.thetechnokid.rw.maths;
 
 public class Force {
+	public static final Force GRAVITY = new Force(5, Direction.SOUTH.clone());
+	
 	private VectorQuantity acceleration;
 	
 	public Force(int magnitude, Direction direction) {
