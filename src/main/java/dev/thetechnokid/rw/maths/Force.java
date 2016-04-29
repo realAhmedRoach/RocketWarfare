0dae9e2c4521144977bd5e023eeb2832bafd679e
theTechnoKid <thetechnokid11@gmail.com>
2016-04-29 13:18:06 -0400

    Ability to get default force

--- src/main/java/dev/thetechnokid/rw/maths/Force.java
+++ src/main/java/dev/thetechnokid/rw/maths/Force.java
@@ -4,22 +4,30 @@ import dev.thetechnokid.rw.utils.Physics;
 
 public class Force {
 	public static final Force GRAVITY = new Force(Physics.G, Direction.SOUTH.clone());
-	
+
 	private VectorQuantity acceleration;
-	
+
 	public Force(double magnitude, Direction direction) {
 		acceleration = new VectorQuantity(magnitude, direction);
 	}
-	
+
 	public VectorQuantity getAcceleration() {
 		return acceleration;
 	}
-	
-	public double getForceY(int time) {
-		return (acceleration.getMagnitude() * acceleration.getDirection().getAltitudeModifier()) * time;
+
+	public double getForceY() {
+		return (acceleration.getMagnitude() * acceleration.getDirection().getAltitudeModifier());
+	}
+
+	public double getForceX() {
+		return (acceleration.getMagnitude() * acceleration.getDirection().getXModifier());
 	}
 	
+	public double getForceY(int time) {
+		 return getForceY() * time;
+	}
+
 	public double getForceX(int time) {
-		return (acceleration.getMagnitude() * acceleration.getDirection().getXModifier()) * time;
+		return getForceX() * time;
 	}
 }
