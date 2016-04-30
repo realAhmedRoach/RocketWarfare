0be9c46021e8aa6d419f13ce8548b8a4600ed92c
theTechnoKid <thetechnokid11@gmail.com>
2016-04-30 06:26:53 -0400

    Accelerated forces

--- src/main/java/dev/thetechnokid/rw/maths/Force.java
+++ src/main/java/dev/thetechnokid/rw/maths/Force.java
@@ -3,18 +3,32 @@ package dev.thetechnokid.rw.maths;
 import dev.thetechnokid.rw.utils.Physics;
 
 public class Force {
-	public static final Force GRAVITY = new Force(Physics.G, Direction.SOUTH.clone());
+	public static final Force GRAVITY = new Force(Physics.G, Direction.SOUTH.clone(), false);
 
 	private VectorQuantity acceleration;
+	private boolean accelerated;
 
-	public Force(double magnitude, Direction direction) {
+	public Force(double magnitude, Direction direction, boolean accelerated) {
 		acceleration = new VectorQuantity(magnitude, direction);
+		this.accelerated = accelerated;
+	}
+	
+	public Force(double magnitude, Direction direction) {
+		this(magnitude, direction, true);
 	}
 
 	public VectorQuantity getAcceleration() {
 		return acceleration;
 	}
 
+	public boolean isAccelerated() {
+		return accelerated;
+	}
+	
+	public void setAccelerated(boolean a) {
+		accelerated = a;
+	}
+	
 	public double getForceY() {
 		return (acceleration.getMagnitude() * acceleration.getDirection().getAltitudeModifier());
 	}
