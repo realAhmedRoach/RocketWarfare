e563b7921fb58d8cfd020c8a20ad2f01b76c7ad1
theTechnoKid <thetechnokid11@gmail.com>
2016-05-12 21:06:49 -0400

    Basic login

--- src/main/java/dev/thetechnokid/rw/states/MenuState.java
+++ src/main/java/dev/thetechnokid/rw/states/MenuState.java
@@ -1,10 +1,22 @@
 package dev.thetechnokid.rw.states;
 
+import java.io.File;
+
+import dev.thetechnokid.rw.RocketWarfare;
 import dev.thetechnokid.rw.controllers.MainGameController;
-import dev.thetechnokid.rw.utils.*;
-import javafx.geometry.*;
+import dev.thetechnokid.rw.net.User;
+import dev.thetechnokid.rw.utils.Grid;
+import dev.thetechnokid.rw.utils.Language;
+import dev.thetechnokid.rw.utils.Utils;
+import javafx.geometry.Point2D;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
@@ -39,23 +51,55 @@ public class MenuState extends State {
 		pwField.setPromptText(Language.get("password"));
 		pwField.setMaxWidth(100);
 
+		CheckBox register = new CheckBox("Register");
+
 		Button start = new Button(Language.get("login"));
 		start.setOnAction((event) -> {
 			name = nameField.getText();
-			textOn = true;
-			gridded = true;
+			if (login(name, pwField.getText(), register.isSelected())) {
+				textOn = true;
+				gridded = true;
+			}
 			start.disableProperty().set(true);
 			g.getCanvas().requestFocus();
 		});
-		
+
 		grid.add(nameLabel, 0, 1);
 		grid.add(nameField, 1, 1);
 		grid.add(pwLabel, 0, 2);
 		grid.add(pwField, 1, 2);
 		grid.add(start, 1, 3);
-
+		grid.add(register, 0, 3);
 
 		MainGameController.buttons().addAll(grid);
+	}
+
+	private boolean login(String name, String password, boolean register) {
+		File file = new File(RocketWarfare.settingsFolder() + "/users/" + name);
+		try {
+			User user = User.load(name, password);
+			if (user == null) {
+				if (register) {
+					if (file.exists()) {
+						MainGameController.setStatus("User with that name already exists!");
+						return false;
+					}
+					MainGameController.get().FIRST_TIME = true;
+					User newUser = new User(name, password);
+					MainGameController.get().USER = newUser;
+					return true;
+				} else {
+					MainGameController.setStatus("Incorrect username of password.");
+					return false;
+				}
+			} else if (user != null) {
+				MainGameController.get().USER = user;
+				MainGameController.setStatus("Login Successfull!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
 	@Override
