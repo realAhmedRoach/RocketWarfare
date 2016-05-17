2699aed892b8124f840e6ebcbb384edeeea08694
theTechnoKid <thetechnokid11@gmail.com>
2016-05-17 09:47:33 -0400

    Offline mode; Add description

--- src/main/java/dev/thetechnokid/rw/Version.java
+++ src/main/java/dev/thetechnokid/rw/Version.java
@@ -1,9 +1,6 @@
 package dev.thetechnokid.rw;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -11,6 +8,9 @@ public class Version {
 	public static final String NAME = "Rocket Warfare";
 	public static final String VERSION = "0.0.1-SNAPSHOT";
 	public static final String FRIENDLY = "Dev Edition";
+	public static final String DESCRIPTION = "This is a really unstable initial development build.";
+
+	private static boolean OFFLINE = false;
 
 	private static final String versionSite = "http://thetechnokid.github.com/RocketWarfare/version.txt";
 	private static ArrayList<String> data;
@@ -26,6 +26,7 @@ public class Version {
 			while ((line = r.readLine()) != null)
 				data.add(line);
 		} catch (IOException e) {
+			OFFLINE = true;
 			e.printStackTrace();
 		}
 	}
@@ -58,14 +59,14 @@ public class Version {
 	}
 
 	public static String getVersion() {
-		return data.get(1);
+		return OFFLINE ? VERSION : data.get(1);
 	}
 
 	public static String getFriendly() {
-		return data.get(2);
+		return OFFLINE ? FRIENDLY : data.get(2);
 	}
 
 	public static String getDescription() {
-		return data.get(3);
+		return OFFLINE ? DESCRIPTION : data.get(3);
 	}
 }
