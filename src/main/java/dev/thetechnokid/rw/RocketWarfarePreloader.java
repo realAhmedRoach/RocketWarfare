378a4f652de2a129d4b01ea4e8a153c024fa788b
theTechnoKid <thetechnokid11@gmail.com>
2016-05-17 20:48:51 -0400

    Finished task completion

--- src/main/java/dev/thetechnokid/rw/RocketWarfarePreloader.java
+++ src/main/java/dev/thetechnokid/rw/RocketWarfarePreloader.java
@@ -1,16 +1,13 @@
 package dev.thetechnokid.rw;
 
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
+import java.util.function.Function;
 
 import javafx.application.Preloader;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+import javafx.fxml.*;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebView;
@@ -22,20 +19,44 @@ public class RocketWarfarePreloader extends Preloader implements Initializable {
 	@FXML
 	private WebView news;
 	@FXML
-	private Label status;
+	private TextArea status;
+	@FXML
+	private Button go;
 	@FXML
 	private ProgressBar progress;
-	@FXML
-	private MenuItem close;
-	@FXML
-	private MenuItem about;
+
+	private ArrayList<Function<Boolean, String>> tasks;
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	private void initTasks() {
+		tasks = new ArrayList<>();
+		tasks.add((stuff) -> {
+			return Version.newVersion() ? "New version found:" : "You have the latest version:";
+		});
+		tasks.add((stuff) -> {
+			return Version.getVersion() + ": " + Version.getFriendly() + "; " + Version.getDescription();
+		});
+		tasks.add((stuff) -> {
+			return "Done!";
+		});
+	}
+
+	private void startTasks() {
+		double i = 0.0;
+		for (Function<Boolean, String> function : tasks) {
+			status.appendText(function.apply(true) + "\n");
+			progress.setProgress(i / tasks.size());
+			i++;
+		}
+	}
+
 	@Override
 	public void start(Stage stage) throws Exception {
+		initTasks();
+
 		BorderPane parent = (BorderPane) FXMLLoader.load(getClass().getResource("fxml/Preloader.fxml"));
 		Scene scene = new Scene(parent);
 
@@ -45,6 +66,8 @@ public class RocketWarfarePreloader extends Preloader implements Initializable {
 		stage.setTitle("Rocket Warfare Launcher");
 		stage.getIcons().add(new Image(RocketWarfare.class.getResourceAsStream("/images/logo.png")));
 		stage.show();
+
+		startTasks();
 	}
 
 	@Override
