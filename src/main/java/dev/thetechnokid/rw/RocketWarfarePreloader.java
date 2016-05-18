bd3a487b4c84f68f48a8509a412b53e2cc14fdc7
theTechnoKid <thetechnokid11@gmail.com>
2016-05-18 15:26:39 -0400

    Put controller in separate class

--- src/main/java/dev/thetechnokid/rw/RocketWarfarePreloader.java
+++ src/main/java/dev/thetechnokid/rw/RocketWarfarePreloader.java
@@ -1,62 +1,21 @@
 package dev.thetechnokid.rw;
 
-import java.net.URL;
-import java.util.*;
-import java.util.function.Function;
-
 import javafx.application.Preloader;
-import javafx.fxml.*;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
-public class RocketWarfarePreloader extends Preloader implements Initializable {
+public class RocketWarfarePreloader extends Preloader {
 	
-	private static final String NEWS = "http://thetechnokid.github.io/RocketWarfare";
-	@FXML
-	private WebView news;
-	@FXML
-	private TextArea status;
-	@FXML
-	private Button go;
-	@FXML
-	private ProgressBar progress;
-
-	private ArrayList<Function<Boolean, String>> tasks;
+	public static final String NEWS = "http://thetechnokid.github.io/RocketWarfare";
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-
-	private void initTasks() {
-		tasks = new ArrayList<>();
-		tasks.add((stuff) -> {
-			return Version.newVersion() ? "New version found:" : "You have the latest version:";
-		});
-		tasks.add((stuff) -> {
-			return Version.getVersion() + ": " + Version.getFriendly() + "; " + Version.getDescription();
-		});
-		tasks.add((stuff) -> {
-			return "Done!";
-		});
-	}
-
-	private void startTasks() {
-		double i = 0.0;
-		for (Function<Boolean, String> function : tasks) {
-			status.appendText(function.apply(true) + "\n");
-			progress.setProgress(i / tasks.size());
-			i++;
-		}
-	}
-
 	@Override
 	public void start(Stage stage) throws Exception {
-		initTasks();
-
 		BorderPane parent = (BorderPane) FXMLLoader.load(getClass().getResource("fxml/Preloader.fxml"));
 		Scene scene = new Scene(parent);
 
@@ -67,11 +26,5 @@ public class RocketWarfarePreloader extends Preloader implements Initializable {
 		stage.getIcons().add(new Image(RocketWarfare.class.getResourceAsStream("/images/logo.png")));
 		stage.show();
 
-		startTasks();
-	}
-
-	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-		news.getEngine().load(NEWS);
 	}
 }
---
+++ src/main/java/dev/thetechnokid/rw/controllers/PreloaderController.java
@@ -0,0 +1,54 @@
+package dev.thetechnokid.rw.controllers;
+
+import java.net.URL;
+import java.util.*;
+import java.util.function.Function;
+
+import dev.thetechnokid.rw.*;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.web.WebView;
+
+public class PreloaderController implements Initializable {
+
+	@FXML
+	private WebView news;
+	@FXML
+	private TextArea status;
+	@FXML
+	private Button go;
+	@FXML
+	private ProgressBar progress;
+
+	private ArrayList<Function<Boolean, String>> tasks;
+
+	private void initTasks() {
+		tasks = new ArrayList<>();
+		tasks.add((stuff) -> {
+			return Version.newVersion() ? "New version found:" : "You have the latest version:";
+		});
+		tasks.add((stuff) -> {
+			return Version.getVersion() + ": " + Version.getFriendly() + "; " + Version.getDescription();
+		});
+		tasks.add((stuff) -> {
+			return "Done!";
+		});
+	}
+
+	private void startTasks() {
+		double i = 0.0;
+		for (Function<Boolean, String> function : tasks) {
+			status.appendText(function.apply(true) + "\n");
+			progress.setProgress(i / tasks.size());
+			i++;
+		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		news.getEngine().load(RocketWarfarePreloader.NEWS);
+		initTasks();
+		startTasks();
+	}
+
+}
