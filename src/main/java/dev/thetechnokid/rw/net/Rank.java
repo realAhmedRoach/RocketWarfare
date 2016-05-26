ccef963d6258241ea4ed8a1d01c295fcbfccd1f1
theTechnoKid <thetechnokid11@gmail.com>
2016-05-25 20:31:02 -0400

    Easier way to find rank number

--- src/main/java/dev/thetechnokid/rw/net/Rank.java
+++ src/main/java/dev/thetechnokid/rw/net/Rank.java
@@ -1,32 +1,27 @@
 package dev.thetechnokid.rw.net;
 
 public enum Rank {
-	NOOB(1, "Noob", "Nb"), AIRMAN(2, "Airman", "Am"), AIRMAN_FIRST_CLASS(3, "Airman First Class",
-			"AmFC"), SENIOR_AIRMAN(4, "Senior Airman", "SrAm"), SERGEANT(5, "Sergeant", "Srgt"), MASTER_SERGEANT(6,
-					"Master Sergeant",
-					"MSrgt"), SENIOR_MASTER_SERGEANT(7, "Senior Master Sergeant", "SrMSrgt"), CHIEF_MASTER_SERGEANT(8,
-							"Chief Master Sergeant", "CMSrgt"), COMMAND_CHIEF_MASTER_SERGEANT(9,
-									"Command Chief Master Sergeant",
-									"CoCMSrgt"), SECOND_LIEUTENANT(10, "Second Lieutenant", "SLt"), FIRST_LIEUTENANT(
-											11, "First Lieutenant", "FLt"), CAPTAIN(12, "Captian", "Cap"), MAJOR(13,
-													"Major", "Mjr"), LIEUTENANT_COLONEL(14, "Lieutenant Colonel",
-															"LtCl"), COLONEL(15, "Colonel", "Cl"), BRIGADIER_GENERAL(16,
-																	"Brigadier General", "BgGnrl"), MAJOR_GENERAL(17,
-																			"Major General",
-																			"MjrGnrl"), LIEUTENANT_GENERAL(18,
-																					"Lieutenant General",
-																					"LtGnrl"), GENERAL(19, "General",
-																							"Gnrl"), DICTATOR(20,
-																									"Dictator", "Dcttr");
+	NOOB("Noob", "Nb"), AIRMAN("Airman", "Am"), AIRMAN_FIRST_CLASS("Airman First Class", "AmFC"), SENIOR_AIRMAN(
+			"Senior Airman",
+			"SrAm"), SERGEANT("Sergeant", "Srgt"), MASTER_SERGEANT("Master Sergeant", "MSrgt"), SENIOR_MASTER_SERGEANT(
+					"Senior Master Sergeant",
+					"SrMSrgt"), CHIEF_MASTER_SERGEANT("Chief Master Sergeant", "CMSrgt"), COMMAND_CHIEF_MASTER_SERGEANT(
+							"Command Chief Master Sergeant",
+							"CoCMSrgt"), SECOND_LIEUTENANT("Second Lieutenant", "SLt"), FIRST_LIEUTENANT(
+									"First Lieutenant",
+									"FLt"), CAPTAIN("Captian", "Cap"), MAJOR("Major", "Mjr"), LIEUTENANT_COLONEL(
+											"Lieutenant Colonel", "LtCl"), COLONEL("Colonel", "Cl"), BRIGADIER_GENERAL(
+													"Brigadier General", "BgGnrl"), MAJOR_GENERAL("Major General",
+															"MjrGnrl"), LIEUTENANT_GENERAL("Lieutenant General",
+																	"LtGnrl"), GENERAL("General",
+																			"Gnrl"), DICTATOR("Dictator", "Dcttr");
 
-	private int num;
 	private String name;
 	private String abbv;
 
 	private static Rank[] vals = values();
 
-	Rank(int num, String name, String abbv) {
-		this.num = num;
+	Rank(String name, String abbv) {
 		this.name = name;
 		this.abbv = abbv;
 	}
@@ -36,7 +31,7 @@ public enum Rank {
 	}
 
 	public int getNum() {
-		return num;
+		return this.ordinal() + 1;
 	}
 
 	public String getName() {
