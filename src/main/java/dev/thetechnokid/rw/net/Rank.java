1bc0417fd76514df5c9663bdec20b798230cc74d
theTechnoKid <thetechnokid11@gmail.com>
2016-06-02 22:19:48 -0400

    Fix enum formatting

--- src/main/java/dev/thetechnokid/rw/net/Rank.java
+++ src/main/java/dev/thetechnokid/rw/net/Rank.java
@@ -1,30 +1,29 @@
 package dev.thetechnokid.rw.net;
 
 public enum Rank {
-	NOOB("Noob", "Nb"), AIRMAN("Airman", "Am"), AIRMAN_FIRST_CLASS("Airman First Class", "AmFC"), SENIOR_AIRMAN(
-			"Senior Airman",
-			"SrAm"), AIR_MARSHAL("Air Marshal", "AMsh"), SENIOR_AIR_MARSHAL("Senior Air Marshal", "SrAMsh"), SERGEANT(
-					"Sergeant", "Srgt"), MASTER_SERGEANT("Master Sergeant", "MSrgt"), SENIOR_MASTER_SERGEANT(
-							"Senior Master Sergeant", "SrMSrgt"), CHIEF_MASTER_SERGEANT("Chief Master Sergeant",
-									"CMSrgt"), COMMAND_CHIEF_MASTER_SERGEANT("Command Chief Master Sergeant",
-											"CoCMSrgt"), SECOND_LIEUTENANT("Second Lieutenant",
-													"SLt"), FIRST_LIEUTENANT("First Lieutenant", "FLt"), CAPTAIN(
-															"Captian",
-															"Cap"), MAJOR("Major", "Mjr"), LIEUTENANT_COLONEL(
-																	"Lieutenant Colonel", "LtCl"), COLONEL("Colonel",
-																			"Cl"), BRIGADIER_GENERAL(
-																					"Brigadier General",
-																					"BgGnrl"), MAJOR_GENERAL(
-																							"Major General",
-																							"MjrGnrl"), LIEUTENANT_GENERAL(
-																									"Lieutenant General",
-																									"LtGnrl"), GENERAL(
-																											"General",
-																											"Gnrl"), LIEUTENANT_DICTATOR(
-																													"Lieutenant Dictator",
-																													"LtDcttr"), DICTATOR(
-																															"Dictator",
-																															"Dcttr");
+	NOOB("Noob", "Nb"),
+	AIRMAN("Airman", "Am"),
+	AIRMAN_FIRST_CLASS("Airman First Class", "AmFC"),
+	SENIOR_AIRMAN("Senior Airman", "SrAm"),
+	AIR_MARSHAL("Air Marshal", "AMsh"),
+	SENIOR_AIR_MARSHAL("Senior Air Marshal", "SrAMsh"),
+	SERGEANT("Sergeant", "Srgt"),
+	MASTER_SERGEANT("Master Sergeant", "MSrgt"),
+	SENIOR_MASTER_SERGEANT("Senior Master Sergeant", "SrMSrgt"),
+	CHIEF_MASTER_SERGEANT("Chief Master Sergeant", "CMSrgt"),
+	COMMAND_CHIEF_MASTER_SERGEANT("Command Chief Master Sergeant", "CoCMSrgt"),
+	SECOND_LIEUTENANT("Second Lieutenant", "SLt"),
+	FIRST_LIEUTENANT("First Lieutenant", "FLt"),
+	CAPTAIN("Captian", "Cap"),
+	MAJOR("Major", "Mjr"),
+	LIEUTENANT_COLONEL("Lieutenant Colonel", "LtCl"),
+	COLONEL("Colonel", "Cl"),
+	BRIGADIER_GENERAL("Brigadier General", "BgGnrl"),
+	MAJOR_GENERAL("Major General", "MjrGnrl"),
+	LIEUTENANT_GENERAL("Lieutenant General", "LtGnrl"),
+	GENERAL("General", "Gnrl"),
+	LIEUTENANT_DICTATOR("Lieutenant Dictator", "LtDcttr"),
+	DICTATOR("Dictator", "Dcttr");
 
 	private String name;
 	private String abbv;
