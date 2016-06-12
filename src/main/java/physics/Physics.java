fd0793d1dd8152ba03e52e8e60e29422a716f161
theTechnoKid <thetechnokid11@gmail.com>
2016-06-12 08:25:37 -0400

    Remove unnecessary methods, Close #29, #33. (Shouldda been closed)

--- src/main/java/physics/Physics.java
+++ src/main/java/physics/Physics.java
@@ -12,36 +12,6 @@ public class Physics {
 	private Physics() {
 	}
 
-	/**
-	 * Equation for rocket altitude
-	 * 
-	 * @param t
-	 *            Time
-	 * @return The position delta
-	 */
-	public static double positionY(int t, Force... forces) {
-		double add = 0.0;
-		for (Force force : forces) {
-			add += force.isAccelerated() ? force.getForceY(t) : force.getForceY();
-		}
-		return add;
-	}
-
-	/**
-	 * Equation for rocket x
-	 * 
-	 * @param t
-	 *            Time
-	 * @return The position delta
-	 */
-	public static double positionX(int t, Force... forces) {
-		double add = 0.0;
-		for (Force force : forces) {
-			add += force.getForceX();
-		}
-		return add;
-	}
-
 	public static void position(int time, Position pos, VectorQuantity velocity, VectorQuantity acceleration,
 			Force... forces) {
 		double netForceY = 0;
