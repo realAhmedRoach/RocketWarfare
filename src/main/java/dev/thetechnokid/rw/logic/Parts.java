b5f4a150a73d6987ce9e231865deb4800ed9d4a3
theTechnoKid <thetechnokid11@gmail.com>
2016-11-01 19:49:16 -0400

    Add new informational enums

---
+++ src/main/java/dev/thetechnokid/rw/logic/Parts.java
@@ -0,0 +1,14 @@
+package dev.thetechnokid.rw.logic;
+
+public enum Parts {
+	NOSE,
+	BODY,
+	WINDOW,
+	DOOR,
+	MISSILEHOLDER,
+	MISSILE,
+	THRUSTER,
+	VENT,
+	FIN,
+	FUELTANK
+}
---
+++ src/main/java/dev/thetechnokid/rw/logic/Tier.java
@@ -0,0 +1,15 @@
+package dev.thetechnokid.rw.logic;
+
+public enum Tier {
+	CAMO,
+	ARMORED,
+	LITE,
+	ELITE,
+	EMPERIAL,
+	REBEL,
+	MAKESHIFT,
+	BOMBER,
+	STEALTH,
+	SUPPLY,
+	SCOUT
+}
