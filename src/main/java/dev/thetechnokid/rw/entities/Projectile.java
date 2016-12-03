74eadf4a02cd61b816230bb1fe1b7f4b9c559cf2
theTechnoKid <theTechnoKid11@gmail.com>
2016-12-03 18:52:49 -0500

    Basic projectile class

---
+++ src/main/java/dev/thetechnokid/rw/entities/Projectile.java
@@ -0,0 +1,19 @@
+package dev.thetechnokid.rw.entities;
+
+import dev.thetechnokid.rw.physics.Physics;
+
+public class Projectile extends FlyingObject {
+
+	private double time;
+
+	@Override
+	public void tick() {
+		pos.y = -Math.pow(Physics.G, 2) * time + velocity.getMagnitude() * time + 0; 
+	}
+
+	@Override
+	public void render(double x, double y) {
+
+	}
+
+}
