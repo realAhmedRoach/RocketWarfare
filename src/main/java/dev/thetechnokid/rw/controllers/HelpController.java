1faf91aa34cc52160c4090e7be379a6286e8ab8e
theTechnoKid <theTechnoKid11@gmail.com>
2017-01-09 16:46:35 -0500

    Help close button is raised

--- src/main/java/dev/thetechnokid/rw/controllers/HelpController.java
+++ src/main/java/dev/thetechnokid/rw/controllers/HelpController.java
@@ -3,21 +3,25 @@ package dev.thetechnokid.rw.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javafx.fxml.*;
-import javafx.scene.control.*;
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 public class HelpController implements Initializable {
 
 	@FXML
 	private Label text;
 	@FXML
-	private Button close;
+	private JFXButton close;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		text.setText("Controls:\n" + "<- & ->: Tilt Rocket\n" + "Up: Increase Acceleration\n" + "Space: Fullscreen\n"
 				+ "Esc: Exit Fullscreen\n\n" + "Menu:\n" + "Change Settings: Edit -> Settings...\n"
 				+ "Guide: Help -> Guide...");
+		close.getStyleClass().add("button-raised1");
 		close.setOnAction(event -> close.getParent().getScene().getWindow().hide());
 	}
 
