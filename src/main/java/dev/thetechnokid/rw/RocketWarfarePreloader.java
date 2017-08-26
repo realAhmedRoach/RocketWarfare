be912c775e0f607873e065ca77965f5d2fe086f8
Ahmed Roach <realahmedroach@gmail.com>
2017-08-26 18:43:38 -0400

    Fix link

--- src/main/java/dev/thetechnokid/rw/RocketWarfarePreloader.java
+++ src/main/java/dev/thetechnokid/rw/RocketWarfarePreloader.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 
 public class RocketWarfarePreloader extends Preloader {
 
-	public static final String NEWS = "http://thetechnokid.github.io/RocketWarfare";
+	public static final String NEWS = "http://realahmedroach.github.io/RocketWarfare";
 	public static boolean DONE = false;
 
 	@Override
